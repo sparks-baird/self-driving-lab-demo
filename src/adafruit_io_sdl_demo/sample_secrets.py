8526c4260ef3af70adeea918f6a7a336e4920617
sgbaird <sterling.baird@utah.edu>
2022-08-30 15:55:33 -0400

    add adafruit items to secrets template

--- src/adafruit_io_sdl_demo/sample_secrets.py
+++ src/adafruit_io_sdl_demo/sample_secrets.py
@@ -1,3 +1,5 @@
-"""WiFi Login Info."""
+"""WiFi and Adafruit IO Login Info. Rename to secrets.py"""
 SSID = "Enter your SSID here"
 PASSWORD = "Enter your WiFi password here"
+ADAFRUIT_IO_USERNAME = "Your account name at io.adafruit.com"
+ADAFRUIT_IO_KEY = "Your API key from io.adafruit.com"
