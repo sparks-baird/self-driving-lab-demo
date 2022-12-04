3c532c207cd4fab73b934e7d2fff6c89c10381b0
sgbaird <sterling.baird@utah.edu>
2022-12-03 23:44:11 -0700

    splice input parameter specific functionality (light and liquid) from API

--- src/self_driving_lab_demo/core.py
+++ src/self_driving_lab_demo/core.py
@@ -20,21 +20,10 @@ References:
     - https://pip.pypa.io/en/stable/reference/pip_install
 """
 import logging
-from importlib.resources import open_text
 from time import sleep
 
 import numpy as np
-import pandas as pd
-from scipy.interpolate import interp1d
-from similaritymeasures import frechet_dist
-from sklearn.metrics import mean_absolute_error, mean_squared_error
 
-from self_driving_lab_demo import data as data_module
-from self_driving_lab_demo.utils.channel_info import (
-    CHANNEL_HEX_COLORS,
-    CHANNEL_NAMES,
-    CHANNEL_WAVELENGTHS_MEAN_FWHM,
-)
 from self_driving_lab_demo.utils.observe import mqtt_observe_sensor_data
 
 __author__ = "sgbaird"
@@ -44,75 +33,14 @@ __license__ = "MIT"
 _logger = logging.getLogger(__name__)
 
 # ---- Python API ----
-# The functions defined in this section can be imported by users in their
-# Python scripts/interactive interpreter, e.g. via
-# `from self_driving_lab_demo.skeleton import fib`,
-# when using this Python module as a library.
-
-wavelength_lbl = "wavelength"  # nm
-intensity_lbl = "relative_intensity"  # (uW/cm^2)/nm
 
 
 class SensorSimulator(object):
     def __init__(self):
-        self.red_interp = self.create_interpolator("neopixel_red.csv")
-        self.green_interp = self.create_interpolator("neopixel_green.csv")
-        self.blue_interp = self.create_interpolator("neopixel_blue.csv")
+        pass
 
-    @property
-    def channel_wavelengths(self):
-        return CHANNEL_WAVELENGTHS_MEAN_FWHM
-
-    @property
-    def channel_hex_colors(self):
-        return CHANNEL_HEX_COLORS
-
-    def create_interpolator(self, fname):
-        df = pd.read_csv(
-            open_text(data_module, fname),
-            header=0,
-            names=[wavelength_lbl, intensity_lbl],
-        )
-
-        df[intensity_lbl].clip(lower=0.0, inplace=True)
-
-        # average y-values for repeat x-values
-        # see also https://stackoverflow.com/a/51258988/13697228
-        df = df.groupby("wavelength", as_index=False).mean()
-
-        return interp1d(
-            df[wavelength_lbl],
-            df[intensity_lbl],
-            kind="linear",
-            bounds_error=False,
-            fill_value=0.0,
-        )
-
-    def _simulate_sensor_data(
-        self, wavelengths, R, G, B, atime=100, astep=999, gain=128
-    ):
-        rI, gI, bI = np.array([R, G, B]) / 255
-
-        # TODO: sample based on Gaussian distributions instead of discrete wavelengths
-
-        wavelengths = np.array(wavelengths)[:, 0]
-
-        channel_data = np.sum(
-            [
-                self.red_interp(wavelengths) * rI,
-                self.green_interp(wavelengths) * gI,
-                self.blue_interp(wavelengths) * bI,
-            ],
-            axis=0,
-        )
-        integration_time = (astep + 1) * (atime + 1) * 2.78 / 1000  # as7341.py
-        channel_data = channel_data * integration_time * gain
-        return {name: data for name, data in zip(CHANNEL_NAMES, channel_data)}
-
-    def simulate_sensor_data(self, R, G, B, atime=100, astep=999, gain=128):
-        return self._simulate_sensor_data(
-            self.channel_wavelengths, R, G, B, atime, astep, gain
-        )
+    def simulate_sensor_data(self, parameters):
+        raise NotImplementedError("This method must be implemented by a subclass.")
 
 
 class SelfDrivingLabDemo(object):
@@ -121,119 +49,65 @@ class SelfDrivingLabDemo(object):
         random_rng=np.random.default_rng(42),
         target_seed=604523,
         rest_seconds=0.0,
-        max_brightness=0.35,
+        max_power=0.35,
         autoload=False,
         simulation=False,
+        simulator=SensorSimulator(),
         observe_sensor_data_fn=mqtt_observe_sensor_data,
         observe_sensor_data_kwargs={},  # dict(PICO_ID="a123b456")
     ):
         self.random_rng = random_rng
         self.target_seed = target_seed
         self.rest_seconds = rest_seconds
-        self.max_brightness = max_brightness
+        self.max_power = max_power
         self.autoload = autoload
         self.simulation = simulation
 
         self.observe_sensor_data_fn = observe_sensor_data_fn
         self.observe_sensor_data_kwargs = observe_sensor_data_kwargs
 
-        self.simulator = SensorSimulator()
+        self.simulator = simulator
 
         if autoload:
             # must come after creating sensor attribute
             self.observe_target_results()
 
-    def observe_sensor_data(self, R, G, B, atime=100, astep=999, gain=128):
+    def observe_sensor_data(self, parameters):
         if self.simulation:
-            return self.simulate_sensor_data(
-                R, G, B, atime=atime, astep=astep, gain=gain
-            )
+            return self.simulate_sensor_data(parameters)
         sleep(self.rest_seconds)
+
         return self.observe_sensor_data_fn(
-            R,
-            G,
-            B,
-            atime=atime,
-            astep=astep,
-            gain=gain,
-            **self.observe_sensor_data_kwargs
+            parameters, **self.observe_sensor_data_kwargs
         )
 
-    def simulate_sensor_data(self, R, G, B, atime=100, astep=999, gain=128):
-        return self.simulator.simulate_sensor_data(
-            R, G, B, atime=atime, astep=astep, gain=gain
-        )
+    def simulate_sensor_data(self, parameters):
+        return self.simulator.simulate_sensor_data(parameters)
 
     def get_random_inputs(self, rng=None):
-        rng = self.random_rng if rng is None else rng
-        # 1.0 is really bright, so no more than `max_brightness`
-        RGB = 255 * rng.random(3) * self.max_brightness
-        R, G, B = np.round(RGB).astype(int)
-        return int(R), int(G), int(B)
+        raise NotImplementedError("Must be implemented by subclass")
 
     @property
     def bounds(self):
-        mx = int(np.round(self.max_brightness * 255))
-        return dict(
-            R=[0, mx],
-            G=[0, mx],
-            B=[0, mx],
-            atime=[0, 255],
-            astep=[0, 65534],
-            gain=[0.5, 512],
-        )
+        raise NotImplementedError("Must be implemented by subclass")
 
     @property
     def parameters(self):
-        return [
-            dict(name=nm, type="range", bounds=bnd) for nm, bnd in self.bounds.items()
-        ]
-
-    @property
-    def channel_names(self):
-        return CHANNEL_NAMES
-
-    @property
-    def channel_wavelengths_mean_fwhm(self):
-        return CHANNEL_WAVELENGTHS_MEAN_FWHM
-
-    @property
-    def channel_wavelengths(self):
-        return [ch[0] for ch in self.channel_wavelengths_mean_fwhm]
+        raise NotImplementedError("Must be implemented by subclass")
 
     def get_target_inputs(self):
         return self.get_random_inputs(np.random.default_rng(self.target_seed))
 
     def observe_target_results(self):
-        self.target_results = self.observe_sensor_data(*self.get_target_inputs())
+        self.target_results = self.observe_sensor_data(self.get_target_inputs())
         if self.target_results is None or (
             self.target_results.get("error", None) is not None
         ):
             raise RuntimeError(self.target_results["error"])
         return self.target_results
 
-    def evaluate(self, R, G, B, atime=100, astep=999, gain=128):
-        if not hasattr(self, "target_results"):
-            raise ValueError(
-                "must call `load_target_data` first or instantiate with autoload=True"
-            )
-        results = self.observe_sensor_data(R, G, B, atime=atime, astep=astep, gain=gain)
-        if results.get("error", None) is not None:
-            raise ValueError(results["error"])
-
-        target_data = [self.target_results[ch] for ch in self.channel_names]
-        data = [results[ch] for ch in self.channel_names]
-
-        results["mae"] = mean_absolute_error(target_data, data)
-        results["rmse"] = mean_squared_error(target_data, data, squared=False)
-
-        target_dist = np.array([self.channel_wavelengths, target_data]).T
-        dist = np.array([self.channel_wavelengths, data]).T
-        results["frechet"] = frechet_dist(target_dist, dist)
-        return results
-
-    def clear(self):
-        self.observe_sensor_data(0, 0, 0)
+    def evaluate(self, parameters):
+        raise NotImplementedError("Must be implemented by subclass")
 
 
 class SDLSimulation(SelfDrivingLabDemo):
