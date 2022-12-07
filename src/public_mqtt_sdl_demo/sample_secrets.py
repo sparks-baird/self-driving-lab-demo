96dde4649e266bed8382e920442263cfcdef83de
sgbaird <sterling.baird@utah.edu>
2022-12-06 23:04:06 -0700

    Update sample_secrets.py

--- src/public_mqtt_sdl_demo/sample_secrets.py
+++ src/public_mqtt_sdl_demo/sample_secrets.py
@@ -3,6 +3,4 @@ SSID = "Enter your SSID here"
 PASSWORD = "Enter your WiFi password here"
 MONGODB_API_KEY = "Enter your Mongo DB API Key (optional)"
 MONGODB_COLLECTION_NAME = "Enter the name of the Mongo DB collection (optional)"
-DEVICE_NICKNAME = (
-    "For logging back to Mongo DB, enter whatever name you want here (optional)"
-)
+DEVICE_NICKNAME = "For MongoDB, enter whatever name you want here (optional)"
