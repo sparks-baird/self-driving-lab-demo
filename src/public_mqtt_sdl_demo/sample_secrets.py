0b70e9aa140e626ebf4cef07a504fff2f2f6f502
sgbaird <sterling.baird@utah.edu>
2022-12-09 16:37:11 -0700

    dummy username, pass, host

--- src/public_mqtt_sdl_demo/sample_secrets.py
+++ src/public_mqtt_sdl_demo/sample_secrets.py
@@ -4,3 +4,6 @@ PASSWORD = "Enter your WiFi password here"
 MONGODB_API_KEY = "Enter your Mongo DB API Key (optional)"
 MONGODB_COLLECTION_NAME = "Enter the name of the Mongo DB collection (optional)"
 DEVICE_NICKNAME = "For MongoDB, enter whatever name you want here (optional)"
+HIVEMQ_USERNAME = "HiveMQ cluster username (optional, recommended for security)"
+HIVEMQ_PASSWORD = "HiveMQ cluster password (optional, recommended for security)"
+HIVEMQ_HOST = "abc1234def4567.s2.eu.hivemq.cloud"  # optional, recommended for security
