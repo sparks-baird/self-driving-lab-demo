0a40250c245f365a345e728430ad2ab426d725ee
sgbaird <sterling.baird@utah.edu>
2022-12-16 13:41:34 -0700

    add returns to separate sections

--- src/public_mqtt_sdl_demo/sample_secrets.py
+++ src/public_mqtt_sdl_demo/sample_secrets.py
@@ -1,9 +1,11 @@
 """WiFi Login Info. Rename this file to secrets.py"""
 SSID = "Enter your SSID here"
 PASSWORD = "Enter your WiFi password here"
+
 MONGODB_API_KEY = "Enter your Mongo DB API Key (optional)"
 MONGODB_COLLECTION_NAME = "Enter the name of the Mongo DB collection (optional)"
 DEVICE_NICKNAME = "For MongoDB, enter whatever name you want here (optional)"
+
 HIVEMQ_USERNAME = "sgbaird"
 HIVEMQ_PASSWORD = "D.Pq5gYtejYbU#L"
 HIVEMQ_HOST = "248cc294c37642359297f75b7b023374.s2.eu.hivemq.cloud"
