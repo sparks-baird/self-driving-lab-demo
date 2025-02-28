1695b26b2ea7cc368927ae7ac1cf8f907f2fbe79
Sterling G. Baird <sterling.baird@utoronto.ca>
2025-02-28 17:20:44 +0000

    Refactor sdl_demo_light_test.py: add print statement for results and clean up commented code

--- tests/sdl_demo_light_test.py
+++ tests/sdl_demo_light_test.py
@@ -111,13 +111,14 @@ def test_public_demo():
     )
     # sleep(10.0)  # hack for troubleshooting stochastic failures
     results = sdl.evaluate(dict(R=10, G=11, B=12))
+    print(results)
     # sleep(10.0)  # hack for troubleshooting stochastic failures
-    # FIXME: check fidelity implementation against microcontroller code and MicroPython output
+    # FIXME: check fidelity implementation against microcontroller code
+    # and MicroPython output
     # fidelity_results = sdl.evaluate(
     #     dict(R=10, G=11, B=12, atime=100 * 2, astep=999 * 2, gain=128 * 2)
     # )
-    print(results)
-    print(fidelity_results)
+    # print(fidelity_results)
 
 
 def test_bad_payload_values():
